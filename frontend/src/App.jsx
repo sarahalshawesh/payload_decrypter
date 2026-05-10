49f7c6cd3af6e942cb846b8ddf8691bd0bfed3b9
Sarah Robinson <sarahrose@Sarahs-MacBook-Pro-8.local>
2026-05-10 17:58:22 +0100

    adds button func to send request to backebd for decrypting payload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,24 +2,49 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-const [fernetKey, setFernetKey] = useState('')
-const [encryptedPayload, setEncryptedPayload] = useState('')
-const [decryptedPayload, setDecryptedPayload] = useState('')
+  const [fernetKey, setFernetKey] = useState('');
+  const [encryptedPayload, setEncryptedPayload] = useState('');
+  const [decryptedPayload, setDecryptedPayload] = useState('');
 
-  return (
-    <main>
-      <h1>Payload Decryption Tool</h1>
-      <form>
-        <label>Payload to decrypt</label>
-        <input 
-        type="text"
-        onChange={(e) => setDecryptedPayload(e.target.value)}></input>
-        <button>
-          Run
-        </button>
-      </form>
-    </main>
-  )
+  async function sendRequest() {  
+    const formData = new FormData();
+    formData.append("encrypted_payload", encryptedPayload);
+    formData.append("fernet_key", fernetKey);
+  }
+
+  try {
+    const res = await fetch("http://127.0.0.1:8000/decrypt", {
+      body: formData, 
+      method: "POST"});
+  }
+  catch (err) {
+    print(err)
+  }
+    return (
+      <main>
+        <h1>Payload Decryption Tool</h1>
+        <form>
+          <label>Payload to decrypt</label>
+          <br></br>
+          <input 
+            type="text"
+            onChange={(e) => setDecryptedPayload(e.target.value)}>
+          </input>
+          <br></br>
+          <label>Key</label>
+          <br></br>
+          <input
+            type="text"
+            onChange={(e) => setFernetKey(e.target.value)}>
+          </input>
+          <br></br>
+          <button
+            onClick={(sendRequest)}>
+            Run
+          </button>
+        </form>
+      </main>
+    )
 }
 
 
