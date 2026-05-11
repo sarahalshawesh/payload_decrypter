de53aad98c2dbfb67703106c2b46144b7b40a709
Sarah Robinson <sarahrose@Sarahs-MacBook-Pro-8.local>
2026-05-11 16:14:30 +0100

    adds CORS handling

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
 from app.routers.decrypt import router as decrypt_router
 from app.routers.encrypt import router as encrypt_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Payload Analysis Tool")
+
+
+
+origins = [
+    "http://localhost", 
+    "http://localhost:5173", 
+    "http://127.0.0.1:5173", 
+]
+
+app.add_middleware(CORSMiddleware, allow_origins = origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
 
 @app.get("/health")
